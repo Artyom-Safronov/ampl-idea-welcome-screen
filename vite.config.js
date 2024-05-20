673c3f39a65602d981f2e02ddb6277f4673ce645
Artyom Safronov <a.safronov@haulmont.net>
2024-05-20 19:03:10 +0400

    in work

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import pugPlugin from "vite-plugin-pug";
 import icons from "./icons.svg";
@@ -6,41 +6,40 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 const options = {};
 const locals = {
-  amplicode_version: process.env.VITE_AMPLICODE_VERSION,
   documentation_and_guides: [
     {
       icon: { src: icons.Globe, alt: "Document icon" },
-      link: { href: "" },
+      link: { href: "https://amplicode.ru/documentation/installation-guide-intellij/" },
       title: "Amlpicode Documentation",
     },
     {
       icon: { src: icons.GitHub, alt: "GitHub icon" },
-      link: { href: "" },
-      title: `What’s New in Amplicode ${process.env.VITE_AMPLICODE_VERSION}`,
+      link: { href: "https://github.com/Amplicode/amplicode-documentation/blob/main/CHANGELOG.md" },
+      title: 'What’s New in Amplicode ${AMPLICODE_VERSION}',
     },
     {
       icon: { src: icons.YouTube, alt: "YouTube icon" },
-      link: { href: "" },
+      link: { href: "https://youtu.be/g5kzePtZ9FQ?si=ZpKogVLi2vwFhFDQ" },
       title: "Amplicode для разработки Spring Boot",
     },
     {
       icon: { src: icons.YouTube, alt: "YouTube icon" },
-      link: { href: "" },
+      link: { href: "https://youtu.be/jX-ap7MYX84?si=drBoYxn0BWm2_cOr" },
       title: "Умное автодополнение derived методов Spring Data Repository",
     },
     {
       icon: { src: icons.YouTube, alt: "YouTube icon" },
-      link: { href: "" },
+      link: { href: "https://youtu.be/w-46xlGRcZk?si=x34-VVttkUCZ1oBE" },
       title: "Flyway + Spring Boot",
     },
     {
       icon: { src: icons.YouTube, alt: "YouTube icon" },
-      link: { href: "" },
+      link: { href: "https://youtu.be/blAaNt_XHAs?si=9I_yptoiluX2DDrW" },
       title: "Liquibase + Spring Boot",
     },
     {
       icon: { src: icons.YouTube, alt: "YouTube icon" },
-      link: { href: "" },
+      link: { href: "https://www.youtube.com/watch?v=jdOQGW6E1Ew" },
       title:
         "Разрабатываем Admin UI на React Admin в VS Code вместе с Amplicode",
     },
@@ -88,17 +87,12 @@ const locals = {
 };
 
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
   return defineConfig({
     plugins: [pugPlugin(options, locals), viteSingleFile()],
     css: {
       postcss: {
         plugins: [tailwindcss()],
       },
-    },
-    define: {
-      "process.env": process.env,
-    },
+    }
   });
 };
